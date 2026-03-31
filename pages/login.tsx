f0daa36ff62a9eee6a8dd2bb20e5c9d15ff08081
abushaid_islam <abushaid_islam@users.noreply.github.com>
2026-04-01 00:36:39 +0600

    error fix

--- pages/login.tsx
+++ pages/login.tsx
@@ -48,7 +48,7 @@ export default function Login() {
     visible: {
       opacity: 1,
       y: 0,
-      transition: { duration: 0.6, ease: easeOut },
+      transition: { duration: 0.6, ease: "easeOut" },
     },
   };
 
